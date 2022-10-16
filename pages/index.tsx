0613a778ff8faef7ec798282faee875abdfeb73f
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2022-10-16 12:10:20 +0300

    banner component added

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Banner from '../components/Banner'
 
 const Home: NextPage = () => {
   return (
@@ -13,6 +14,12 @@ const Home: NextPage = () => {
       {/* Header */}
       <Header/>
       {/* Banner */}
+      <Banner/>
+      <main>
+        <section className=' pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'> Explore Nearby</h2>
+        </section>
+      </main>
      </div>
   )
 }
